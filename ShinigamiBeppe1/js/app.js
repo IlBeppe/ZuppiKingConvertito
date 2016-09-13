9d33d2f56c0e922e7f617927c4f78db5c57c08b3
IlBeppe <gmanfre.gm@gmail.com>
2016-09-13 14:56:05 +0200

    Add files via upload

--- ShinigamiBeppe1/js/app.js
+++ ShinigamiBeppe1/js/app.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
 	$('a[data-rel^=lightcase]').lightcase();
 });
+//$(window).on('resize', function () {
+  //  $('.bg-home').toggleClass('.screen-background', $(window).width() < 768);
+//});
